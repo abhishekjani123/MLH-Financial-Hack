e86d80d0953166c5df2d63e9fc724b1516b52142
abhishekjani123 <abhishekjani123@users.noreply.github.com>
2021-04-04 17:58:54 +0530

    Add project files.

---
+++ Bank.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_4
+{
+    public class Bank
+    {
+        double bal = 0, amt;
+        int acc,credit_count=0,debit_count=0,balance_count=0,fail_count=0;
+        int tt,st;
+        String n;
+        public void Credit(double amount)
+        {
+            amt = amount;
+            if (amt <= 0)
+            {
+                Console.WriteLine("Sorry,Amount can't be deposited");
+                fail_count++;
+            }
+            else
+            {
+                bal = bal + amt;
+                Console.WriteLine("---------------Chimanlal Bank---------------");
+                Console.WriteLine("Amount Deposited");
+                Console.WriteLine("Total Available Balance: " + bal + "Rs");
+                Console.WriteLine("Thank You for Using our Banking Application");
+                Console.WriteLine("--------------------------------------------");
+                DateTime date = DateTime.Now;
+                Console.WriteLine("Digital Copy Generated on:" + date);
+                Console.WriteLine("Branch: Gandhinagar");
+                Console.WriteLine("--------------------------------------------");
+                credit_count++;
+            }
+        }
+        public void Debit(double amount)
+        {
+            amt = amount;
+            if (amt <= 0 || amt > bal)
+            {
+                Console.WriteLine("Sorry,Amount can't be withdraw");
+                fail_count++;
+            }
+            else
+            {
+                bal = bal - amt;
+                Console.WriteLine("---------------Chimanlal Bank---------------");
+                Console.WriteLine("Take Your Bucks: " + amt + "Rs");
+                Console.WriteLine("Total Available Balance: " + bal + "Rs");
+                Console.WriteLine("Thank You for Using our Banking Application");
+                Console.WriteLine("--------------------------------------------");
+                DateTime date = DateTime.Now;
+                Console.WriteLine("Digital Copy Generated on:" + date);
+                Console.WriteLine("Branch: Gandhinagar");
+                Console.WriteLine("--------------------------------------------");
+                debit_count++;
+            }
+        }
+        public void Balance_check(String name, int account)
+        {
+            n = name;
+            acc = account;
+            Console.WriteLine("---------------Chimanlal Bank---------------");
+            Console.WriteLine("Name: " + n);
+            Console.WriteLine("Account Number: " + acc);
+            Console.WriteLine("Your Available Balance: " + bal + "Rs");
+            Console.WriteLine("Thank You for Using our Banking Application");
+            Console.WriteLine("--------------------------------------------");
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Digital Copy Generated on:" + date);
+            Console.WriteLine("Branch: Gandhinagar");
+            Console.WriteLine("--------------------------------------------");
+            balance_count++;
+        }
+
+        public void Get_info(String name, int account)
+        {
+            n = name;
+            acc = account;
+            tt = fail_count + debit_count + credit_count + balance_count;
+            st = debit_count + credit_count + balance_count;
+            Console.WriteLine("---------------Chimanlal Bank---------------");
+            Console.WriteLine("Name: " + n);
+            Console.WriteLine("Account Number: " + acc);
+            Console.WriteLine("Your Available Balance: " + bal + "Rs");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Transaction Summary:");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("NUM |      PROCESS    |  COUNT |");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("<1> |      CREDIT     |  "+credit_count+"     |");
+            Console.WriteLine("<2> |      DEBIT      |  "+  debit_count+"     |");
+            Console.WriteLine("<3> | BALANCE INQUIRY |  "+  balance_count+"     |");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Successful Transaction:" + st);
+            Console.WriteLine("Fail Transaction:" + fail_count);
+            Console.WriteLine("Total Transaction: " + tt);
+            Console.WriteLine("Thank You for Using our Banking Application");
+            Console.WriteLine("--------------------------------------------");
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Digital Copy Generated on:" + date);
+            Console.WriteLine("Branch: Gandhinagar");
+            Console.WriteLine("--------------------------------------------");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Practical_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pin = 1234;
+            int account, pin1, c;
+            double amount;
+            Bank b = new Bank();
+            Console.WriteLine("\t\t\t\t\t\t----- Chimanlal Co.op Bank -----");
+            Console.WriteLine("Enter Your Name:");
+            string name= Console.ReadLine();
+            Console.WriteLine("Enter your Account Number:");
+            account = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Pin:");
+            pin1 = Convert.ToInt32(Console.ReadLine());
+            if (pin1 == pin)
+            {
+                Console.WriteLine("Welcome " + name);
+                Console.WriteLine("Account Number:" + account);
+                Console.WriteLine("1.To Deposit");
+                Console.WriteLine("2.To Withdrawn");
+                Console.WriteLine("3.To Balace Inqiry");
+                Console.WriteLine("4.To Account Details");
+                Console.WriteLine("5.To Exit");
+                while (true)
+                {
+                    Console.WriteLine("Enter Your Choice:");
+                    c = Convert.ToInt32(Console.ReadLine());
+                    switch (c)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter Amount to be Deposited:");
+                            amount = Convert.ToDouble(Console.ReadLine());
+                            b.Credit(amount);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter Amount to be Withdrawn:");
+                            amount = Convert.ToDouble(Console.ReadLine());
+                            b.Debit(amount);
+                            break;
+                        case 3:
+                            b.Balance_check(name, account);
+                            break;
+                        case 4:
+                           b.Get_info(name, account);
+                            break;
+                        case 5:
+                            System.Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong Pin");
+            }
+        }
+        
+    }
+}
